43c66eb7b79ac10107f62e4c35ce309e3d5f3f49
Vincent Kioko <vincentkioko51@gmail.com>
2025-04-11 22:21:24 +0300

    Update lib/util.ts

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,7 +1,7 @@
-import * as crypto from "node:crypto";
+import { createHash } from "crypto";
 
 export const getGravatarUrl = (email: string, size = 80) => {
     const trimmedEmail = email.trim().toLowerCase();
-    const hash = crypto.createHash("md5").update(trimmedEmail).digest("hex");
+    const hash = createHash("md5").update(trimmedEmail).digest("hex");
     return `https://www.gravatar.com/avatar/${hash}?s=${size}&d=wavatar&r=r`;
 }
